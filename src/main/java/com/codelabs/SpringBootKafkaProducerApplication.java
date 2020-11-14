715b16861c734d1df4435d9d22d1e93ba5fbe5e4
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-14 21:16:24 +0530

    Produced Json Object Instead of String

--- src/main/java/com/codelabs/SpringBootKafkaProducerApplication.java
+++ src/main/java/com/codelabs/SpringBootKafkaProducerApplication.java
@@ -1,7 +1,19 @@
 package com.codelabs;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.codelabs.model.Employee;
 
 @SpringBootApplication
 public class SpringBootKafkaProducerApplication {
@@ -9,5 +21,18 @@ public class SpringBootKafkaProducerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKafkaProducerApplication.class, args);
 	}
-
+ 
+	@Bean 
+	public ProducerFactory<String, Employee> producerFactory(){
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);		
+		return new DefaultKafkaProducerFactory<>(configs);
+	}
+	
+	@Bean
+	public KafkaTemplate<String, Employee> kafkaTemplate(){
+		return new KafkaTemplate<>(producerFactory());
+	}
 }
--- src/main/java/com/codelabs/controller/ProducerResource.java
+++ src/main/java/com/codelabs/controller/ProducerResource.java
@@ -4,21 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codelabs.model.Employee;
 
 @RestController
 @RequestMapping("/api/kafka/producer")
 public class ProducerResource {
 
 	@Autowired
-	KafkaTemplate<String, String> kafkaTemplate;
+	KafkaTemplate<String, Employee> kafkaTemplate;
 	
 	private final String TOPIC_NAME = "firstTopic";
 	
-	@GetMapping("{message}")
-	public String produceMessage(@PathVariable("message") final String message) {
-		kafkaTemplate.send(TOPIC_NAME, message);
-		return "Produced Successfully";
+	@PostMapping
+	public String produceMessage(@RequestBody Employee employee) {
+		kafkaTemplate.send(TOPIC_NAME, employee);
+		return "Message Produced Successfully";
 	}
 }
