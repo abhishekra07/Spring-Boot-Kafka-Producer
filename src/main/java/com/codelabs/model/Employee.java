0a9127ce467849a233cdb6d8ee0e95e0aec6d319
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-14 21:00:03 +0530

    added new model

---
+++ src/main/java/com/codelabs/model/Employee.java
@@ -0,0 +1,40 @@
+package com.codelabs.model;
+
+public class Employee {
+
+	private String name;
+	private String age;
+	private String department;
+	private Long salary;
+	private String phoneNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public Long getSalary() {
+		return salary;
+	}
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
